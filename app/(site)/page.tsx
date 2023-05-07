fae04c9d317c2416062389aa08ea5d66c93e1878
jhed-98 <cod.mobile.jhed@gmail.com>
2023-05-07 16:57:54 -0500

    change frontend singin and install prisma

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <p>
+                Hello
+            </p>
+        </div>
+    )
+}
