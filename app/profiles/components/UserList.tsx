64d17f0e63a7d6b29c2040992cc21f09d8168055
jhed-98 <cod.mobile.jhed@gmail.com>
2023-05-10 12:49:39 -0500

    userlist

--- app/profiles/components/UserList.tsx
+++ app/profiles/components/UserList.tsx
@@ -22,7 +22,7 @@ const UserList: React.FC<UserListProps> = ({ currentUser }) => {
             <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
                 <img draggable={false} className="w-max h-max object-contain" src={imgSrc} alt="" />
             </div>
-            <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">{currentUser.name}</div>
+            <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">{currentUser?.name}</div>
         </div>
     )
 }
